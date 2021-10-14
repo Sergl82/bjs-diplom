59e540a738dc552d3d69d08070a8a47a18e2afe5
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-10-14 21:38:12 +1100

    Diplom

---
+++ public/homePage.js
@@ -0,0 +1,107 @@
+"use strict"
+
+//Выход из профиля
+const logoutButton = new LogoutButton();
+  logoutButton.action = () => {
+    ApiConnector.logout(response => {
+      if(response.success){
+        location.reload();
+      }
+    })
+  }
+
+  //Информация о пользователе
+  ApiConnector.current(response => {
+    if(response.success){
+      ProfileWidget.showProfile(response.data);
+    }
+  })
+
+//Обновление курса валют
+const ratesBoard = new RatesBoard();
+const exchangeRates = () => {
+  ApiConnector.getStocks(response => {
+    if (response.success){
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }
+  });
+};
+exchangeRates();
+
+setInterval(exchangeRates, 60000);
+
+//Операции с валютой
+//Пополнение счета
+const moneyManager = new MoneyManager();
+  moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, response => {
+      if (response.success){
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, 'Пополнение счета успешно!');
+      }
+      else {
+        moneyManager.setMessage(response.success, 'Ошибка! Попробуйте еще раз!');
+      }
+    })
+  }
+// Конвертация валют
+  moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, response => {
+      if (response.success){
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, 'Операция прошла успешно!')
+      }
+      else {
+        moneyManager.setMessage(response.success, 'Ошибка! Попробуйте еще раз!')
+      }
+    })
+  }
+
+  moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, 'Перевод выполнен успешно!');
+        }
+        else {
+            moneyManager.setMessage(response.success, 'Ошибка! Попробуйте еще раз!');
+        }
+    });
+};
+
+  //Работа с избранным
+const favoritesWidget = new FavoritesWidget();
+const getFavorites = () => {
+    ApiConnector.getFavorites(response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+    });
+};
+getFavorites();
+
+    favoritesWidget.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            moneyManager.setMessage(response.success, 'Пользователь добавлен в список!');
+            getFavorites();
+        }
+        else {
+            moneyManager.setMessage(response.success, 'Ошибка! Попробуйте еще раз!');
+        }
+    });
+};
+    favoritesWidget.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            moneyManager.setMessage(response.success, 'Пользователь удален из списка!');
+            getFavorites();
+        }
+        else {
+            moneyManager.setMessage(response.success, 'Ошибка! Попробуйте еще раз!');
+        }
+    });
+};
---
+++ public/loginPage.js
@@ -0,0 +1,25 @@
+"use strict"
+ const userForm = new UserForm();
+  userForm.loginFormCallback = data => {
+    ApiConnector.login(data, response => {
+      if(!response.success){
+        userForm.setLoginErrorMessage(response.error);
+      }
+      else {
+        location.reload();
+      }
+
+    })
+  }
+
+  userForm.registerFormCallback = data => {
+    ApiConnector.register(data, response => {
+      if(!response.success) {
+        userForm.setLoginErrorMessage(response.error);
+      }
+      else {
+        location.reload();
+      }
+    })
+  }
+
